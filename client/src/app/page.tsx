e9ca3213d453ee37cee8dba804fce8daac5b45cd
Vman1907 <varshmaan.sonkar@gmail.com>
2025-08-07 22:31:51 +0530

    refactor: update layout and enhance visuals in Home component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,8 +10,8 @@ export default async function Home() {
 			<Navbar />
 			<div className='relative flex min-h-screen flex-col justify-center overflow-hidden w-full rounded-md z-0'>
 				<div className='relative flex w-full flex-1 justify-center isolate z-0 flex-col'>
-					<div className='grid grid-cols-2 gap-4 w-full px-4 md:px-8 items-center justify-center'>
-						<div>
+					<div className='grid grid-cols-1 md:grid-cols-3 gap-4 w-full px-4 md:px-8 items-center justify-center'>
+						<div className='col-span-2'>
 							<div className='text-4xl font-medium mb-8'>
 								<p>Priorities your health Experience</p>
 								<p>with Exceptional Care.</p>
@@ -21,43 +21,44 @@ export default async function Home() {
 								tests to home nursing and OPD consultations. Convenient, reliable, and trusted by
 								1345+ happy patients.
 							</p>
-						</div>
-						<div>
-							<Image src={HERO_IMAGE} width={540} height={540} alt='' />
-						</div>
-					</div>
-					<div className='flex flex-col md:flex-row items-center justify-start gap-4 mt-8 ml-8'>
-						<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
-							<div className='flex flex-col items-center justify-center gap-2 p-4'>
-								<div className='text-sm'>Doctor Home Visit</div>
-								<Button className='!py-0 px-4 h-min'>Book Now</Button>
+							<div className='flex flex-col md:flex-row items-center justify-start gap-4 mt-8'>
+								<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
+									<div className='flex flex-col items-center justify-center gap-2 p-4'>
+										<div className='text-sm'>Doctor Home Visit</div>
+										<Button className='!py-0 px-4 h-min'>Book Now</Button>
+									</div>
+									<div className='w-full aspect-video bg-red-500'></div>
+									<img src='' />
+								</div>
+								<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
+									<div className='flex flex-col items-center justify-center gap-2 p-4'>
+										<div className='text-sm'>Doctor Home Visit</div>
+										<Button className='!py-0 px-4 h-min'>Book Now</Button>
+									</div>
+									<div className='w-full aspect-video bg-red-500'></div>
+									<Image src='' alt='' />
+								</div>
+								<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
+									<div className='flex flex-col items-center justify-center gap-2 p-4'>
+										<div className='text-sm'>Doctor Home Visit</div>
+										<Button className='!py-0 px-4 h-min'>Book Now</Button>
+									</div>
+									<div className='w-full aspect-video bg-red-500'></div>
+									<img src='' />
+								</div>
+								<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
+									<div className='flex flex-col items-center justify-center gap-2 p-4'>
+										<div className='text-sm'>Doctor Home Visit</div>
+										<Button className='!py-0 px-4 h-min'>Book Now</Button>
+									</div>
+									<div className='w-full aspect-video bg-red-500'></div>
+									<img src='' />
+								</div>
 							</div>
-							<div className='w-full aspect-video bg-red-500'></div>
-							<img src='' />
 						</div>
-						<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
-							<div className='flex flex-col items-center justify-center gap-2 p-4'>
-								<div className='text-sm'>Doctor Home Visit</div>
-								<Button className='!py-0 px-4 h-min'>Book Now</Button>
-							</div>
-							<div className='w-full aspect-video bg-red-500'></div>
-							<Image src='' alt='' />
-						</div>
-						<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
-							<div className='flex flex-col items-center justify-center gap-2 p-4'>
-								<div className='text-sm'>Doctor Home Visit</div>
-								<Button className='!py-0 px-4 h-min'>Book Now</Button>
-							</div>
-							<div className='w-full aspect-video bg-red-500'></div>
-							<img src='' />
-						</div>
-						<div className='flex flex-col items-center justify-center shadow-lg rounded-lg bg-white overflow-hidden'>
-							<div className='flex flex-col items-center justify-center gap-2 p-4'>
-								<div className='text-sm'>Doctor Home Visit</div>
-								<Button className='!py-0 px-4 h-min'>Book Now</Button>
-							</div>
-							<div className='w-full aspect-video bg-red-500'></div>
-							<img src='' />
+						<div className='relative'>
+							<Image src={HERO_IMAGE} width={390} height={540} alt='' />
+							<div className='absolute -z-10 right-0 bottom-0 h-[70%] w-[50%] bg-primary rounded-xl'></div>
 						</div>
 					</div>
 				</div>
