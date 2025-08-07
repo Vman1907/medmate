ebf8ea00abcdb7a837d707ed0aa44648c8c8253b
Sanskar <64313600+sanskari27@users.noreply.github.com>
2025-08-07 22:10:00 +0530

    feat: add sleek logo images and update footer component
    - Introduced two new SVG logo files: sleek-logo.svg and sleek-logo-light.svg for branding.
    - Updated the Footer component to include the sleek logo and adjusted layout for better visual appeal.
    - Enhanced footer links and button text for improved clarity and user experience.

--- client/src/components/elements/footer.tsx
+++ client/src/components/elements/footer.tsx
@@ -1,38 +1,33 @@
+import { SLEEK_LOGO_WHITE } from '@/lib/consts';
+import { Phone } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 
 export default function Footer() {
 	return (
-		<footer className='bg-primary text-accent px-[7%]'>
-			<div className='pt-16 pb-4 px-4%'>
-				<div className='w-full flex flex-col md:flex-row text-center md:text-left gap-12 md:gap-8 py-[5rem]'>
-					<div className='w-full md:w-1/5 flex flex-col text-center items-center md:items-start md:text-left'>
-						<p className='underline underline-offset-8 font-medium'>Menu</p>
-						<div className='mt-4'>
-							<Link className='hover:text-slate-200' href='/terms'>
-								<p>Home</p>
-							</Link>
-							<Link className='hover:text-slate-200' href='/terms'>
-								<p>About Us</p>
-							</Link>
-							<Link className='hover:text-slate-200' href='/privacy'>
-								<p>Services</p>
-							</Link>
-							<Link className='hover:text-slate-200' href='/disclaimer'>
-								<p>Contact Us</p>
-							</Link>
-						</div>
+		<footer className='bg-gradient-to-b from-transparent from-0% via-primary/50 via-40% to-primary text-accent px-[5%] pt-[7rem]'>
+			<div className='pt-16 pb-4 px-[7%]'>
+				<div className='w-full flex flex-col md:flex-row text-center md:text-left gap-12 md:gap-8 py-[1rem] justify-between'>
+					<div className='w-full md:w-1/5 flex flex-col text-center items-center justify-center md:items-start md:text-left'>
+						<Image
+							src={SLEEK_LOGO_WHITE}
+							alt='Medmate'
+							width={400}
+							height={250}
+							className='text-white'
+						/>
 					</div>
 					<div className='w-full md:w-1/5 flex flex-col items-center md:items-start text-center md:text-left gap-0'>
-						<p className='underline underline-offset-8 font-medium'>Link</p>
+						<p className='underline underline-offset-8 font-medium'>Explore</p>
 						<div className='mt-4'>
-							<Link className='hover:text-slate-200' href='/terms'>
-								<p>Home</p>
-							</Link>
 							<Link className='hover:text-slate-200' href='/terms'>
 								<p>Home Visit</p>
 							</Link>
 							<Link className='hover:text-slate-200' href='/privacy'>
+								<p>Consultation</p>
+							</Link>
+							<Link className='hover:text-slate-200' href='/privacy'>
 								<p>Services</p>
 							</Link>
 							<Link className='hover:text-slate-200' href='/disclaimer'>
@@ -40,8 +35,11 @@ export default function Footer() {
 							</Link>
 						</div>
 					</div>
-					<div className='w-full md:w-2/5 flex flex-col items-center md:items-start text-center md:text-left gap-0'>
-						<p className='underline underline-offset-8 font-medium'>Get in touch</p>
+					<div className='w-full md:w-2/5 flex flex-col items-center md:items-start text-center md:text-left'>
+						<Button variant={'secondary'} className='pr-8 pl-10 relative group '>
+							Book A Consultation
+							<Phone className='w-4 h-4 absolute left-4 top-1/2 -translate-y-1/2 group-hover:translate-x-1 transition-all duration-300' />
+						</Button>
 						<div className='mt-4'>
 							<p>Stellar Coaching & Consulting</p>
 							<p>B-502, Sahara Apartment, Plot No. 11,</p>
@@ -49,20 +47,9 @@ export default function Footer() {
 							<p>New Delhi, Delhi 110075, India</p>
 						</div>
 					</div>
-					<div className='w-full md:w-1/5 flex flex-col items-center md:items-start text-center md:text-left gap-0'>
-						<Button variant={'secondary'} className='px-8'>
-							Book A Consultation
-						</Button>
-					</div>
 				</div>
-				{/* <div className='flex justify-start items-center mt-8'>
-					<Facebook />
-					<Twitter className='ml-4' />
-					<Linkedin className='ml-4' />
-					<Instagram className='ml-4' />
-				</div> */}
 				<div className='flex flex-col md:flex-row items-center justify-center mt-8 w-full'>
-					<p className='px-4 text-center'>Copyright ©</p>
+					<p className='px-4 text-center'>Copyright © medmate.in</p>
 				</div>
 			</div>
 		</footer>
--- client/src/lib/consts.ts
+++ client/src/lib/consts.ts
@@ -1,6 +1,8 @@
 export const LOGO_WHITE = '/images/logo-white.svg';
 export const LOGO_PRIMARY = '/images/logo-primary.png';
 export const NOT_FOUND = '/images/not-found.svg';
+export const SLEEK_LOGO = '/images/sleek-logo.svg';
+export const SLEEK_LOGO_WHITE = '/images/sleek-logo-light.svg';
 export const SERVER_URL = process.env.NEXT_PUBLIC_API_URL;
 export const HERO_IMAGE = '/images/hero_doctor.png';
 
