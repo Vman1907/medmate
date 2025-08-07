14d567cf506377eebf693ed49af8d49d2d885e97
Sanskar <64313600+sanskari27@users.noreply.github.com>
2025-08-08 00:09:07 +0530

    refactor: remove unused image URL from page component to streamline code

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,9 +10,6 @@ import {
 } from '@/lib/consts';
 import Image from 'next/image';
 
-const imageUrl =
-	'https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
 const people = [
 	{
 		id: 1,
